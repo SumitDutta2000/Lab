56d11adfdde7f9f38c67121c434fbedc2efef7ea
SUMIT DUTTA <107988864+SumitDutta2000@users.noreply.github.com>
2022-06-24 11:14:27 +0530

    Update Addtwonumbers.java

--- Addtwonumbers.java
+++ Addtwonumbers.java
@@ -1,10 +1,19 @@
+// tto print addition of two numbers
+
 class Addtwonumbres {
 
    public static void main(String[] args) {
+      
+      // variables
         
       int a = 20;
       int b = 45;
+      
+      //addition
+      
       int sum = a+b;
+      
+      // to print the sum of a & b
 
       System.out.println("Sum of a & b numbers: "+sum);
    }
