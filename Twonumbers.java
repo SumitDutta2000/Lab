171cefcdf39817bcbec58d7b67d783a815f66f75
SUMIT DUTTA <107988864+SumitDutta2000@users.noreply.github.com>
2022-06-24 11:22:46 +0530

    Update Twonumbers.java

--- Twonumbers.java
+++ Twonumbers.java
@@ -1,11 +1,21 @@
- class Twonumbers {
+// to print multiplication of two numbers 
+
+class Twonumbers {
    public static void main(String[] args) {
+    
+       // variables
        int a;
        int b;
        int c;
        a = 5;
        b = 58;
+    
+       // multiplication
+    
        c = a*b; 
+    
+       // to print the multiplication of a & b
+    
        System.out.println("5*58 = " + c);
    }
 }
