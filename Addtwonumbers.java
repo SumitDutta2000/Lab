92a8c739620318fbafc43d15ab79e133b793f2a7
SUMIT DUTTA <107988864+SumitDutta2000@users.noreply.github.com>
2022-06-24 07:53:43 +0530

    Add files via upload

---
+++ Addtwonumbers.java
@@ -0,0 +1,11 @@
+class Addtwonumbres {
+
+   public static void main(String[] args) {
+        
+      int a = 20;
+      int b = 45;
+      int sum = a+b;
+
+      System.out.println("Sum of a & b numbers: "+sum);
+   }
+}
