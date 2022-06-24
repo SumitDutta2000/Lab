39d06aba1b33b2db28bf468c7e343626529daf36
SUMIT DUTTA <107988864+SumitDutta2000@users.noreply.github.com>
2022-06-24 07:54:17 +0530

    Add files via upload

---
+++ Multiplytwonumber.java
@@ -0,0 +1,12 @@
+class Multiplytwonumber{
+
+    public static void main(String[] args) {
+
+        float first = 2.5f;
+        float second = 3.5f;
+
+        float product = first * second;
+
+        System.out.println("The multiply of Two Number is: " + product);
+    }
+}
