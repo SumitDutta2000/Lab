180f7bfbcdeddb29abf964c141e4bf13826d3677
SUMIT DUTTA <107988864+SumitDutta2000@users.noreply.github.com>
2022-07-15 10:55:43 +0530

    Add files via upload

---
+++ PatternPrinting.java
@@ -0,0 +1,28 @@
+
+/*print this pattern:
+	54321
+	5432
+	543
+	54
+	5
+	  */
+	
+
+package basicprogram;
+
+public class PatternPrinting {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++)//outer loop
+		{
+			for(int j=5;j>=i;j--)//inner loop
+			{
+				System.out.print(j);// print the value of j
+			}
+			System.out.println();//print in next row
+		}
+	}
+
+	}
+
+
